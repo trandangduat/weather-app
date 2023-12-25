2a4a1f246ac7eb4e487b37a24d2bec0a5b8ce350
Tran Dang Duat <23020023@vnu.edu.vn>
2023-12-25 11:00:47 +0700

    Add ASCII banner + minor fixes

--- src/index.js
+++ src/index.js
@@ -2,27 +2,34 @@ import "./style.css";
 
 const cityForm = document.querySelector("form");
 const commandLine = document.querySelector(".line.command");
-const loadingStages = ["|", "/", "-", "\\"];
+const loadingStages = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]; 
 
 cityForm.addEventListener("submit", process);
 
-function newLine (contents, prefix = "", isCommand = 0) {
+function newLine (contents, prefix = "", isCommand = 0, isError = 0) {
     const line = document.createElement("div");
     line.setAttribute("class", "line");
     if (isCommand) {
         line.classList.add("command");
     }
-    line.innerHTML = `<strong class = "instruct">${prefix}</strong> ${contents}`;
+    if (prefix) { 
+        line.innerHTML = `<strong class = ${(isError ? "error": "instruct")}>${prefix}</strong>`;
+    }
+    line.innerHTML += contents;
     return line;
 }
 
 async function process (event) {
     event.preventDefault();
     const cityName = event.target.querySelector("input").value;
+    document.body.insertBefore(newLine(cityName, "", 1), commandLine);
+    cityForm.reset();
 
     // Appending the loading symbol next to the command form when starting fetching
-    const loadingSymbol = document.createElement("span");
-    commandLine.appendChild(loadingSymbol);
+    const loadingSymbol = document.createElement("div");
+    loadingSymbol.setAttribute("class", "line warning");
+    document.body.insertBefore(loadingSymbol, commandLine);
+
     let currentLoadingStage = 0;
     let loadingInterval = setInterval(() => {
         loadingSymbol.textContent = loadingStages[(++currentLoadingStage) % loadingStages.length]
@@ -33,20 +40,17 @@ async function process (event) {
     
     // Remove the loading effect when done fetching 
     clearInterval(loadingInterval);
-    loadingSymbol.textContent = ""; 
+    loadingSymbol.remove();
 
     // Adding result as separate lines 
-    document.body.insertBefore(newLine(cityName, "", 1), commandLine);
     
     if (response.location) {
         document.body.insertBefore(newLine(response.location.name, "name"), commandLine);
         document.body.insertBefore(newLine(response.current.temp_c, "temp"), commandLine);
     } else {
-        document.body.insertBefore(newLine(response, "[ERROR]"), commandLine);
+        document.body.insertBefore(newLine(response, "[ERROR]", 0, 1), commandLine);
     }
     
-
-    cityForm.reset();
 }
 
 async function fetchWeather(CITY_NAME) {

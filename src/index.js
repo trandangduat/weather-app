c3956391c484b35ee400737f6ad451e32ca0cea8
Tran Dang Duat <23020023@vnu.edu.vn>
2023-12-25 14:07:56 +0700

    Forecast added

--- src/index.js
+++ src/index.js
@@ -6,23 +6,23 @@ const loadingStages = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "
 
 cityForm.addEventListener("submit", process);
 
-function newLine (contents, prefix = "", isCommand = 0, isError = 0) {
+function newLine (contents, prefix = "", isCommand = 0, prefixType = "instruct") {
     const line = document.createElement("div");
     line.setAttribute("class", "line");
     if (isCommand) {
         line.classList.add("command");
     }
     if (prefix) { 
-        line.innerHTML = `<strong class = ${(isError ? "error": "instruct")}>${prefix}</strong>`;
+        line.innerHTML = `<strong class = ${prefixType}>${prefix}</strong>`;
     }
     line.innerHTML += contents;
-    return line;
+    document.body.insertBefore(line, commandLine); 
 }
 
 async function process (event) {
     event.preventDefault();
     const cityName = event.target.querySelector("input").value;
-    document.body.insertBefore(newLine(cityName, "", 1), commandLine);
+    newLine(cityName, "", 1);
     cityForm.reset();
 
     // Appending the loading symbol next to the command form when starting fetching
@@ -45,16 +45,25 @@ async function process (event) {
     // Adding result as separate lines 
     
     if (response.location) {
-        document.body.insertBefore(newLine(response.location.name, "name"), commandLine);
-        document.body.insertBefore(newLine(response.current.temp_c, "temp"), commandLine);
+        newLine(response.location.name, "name");
+        newLine(response.current.temp_c, "temp");
+        newLine("", "forecast");
+        response.forecast.forecastday[0].hour.forEach((hour) => {
+            const time = hour.time.split(" ")[1];
+            const temp = hour.temp_c;
+            if (hour.time_epoch >= response.location.localtime_epoch) {
+                newLine(temp, time, 0, "note")
+            }
+        });  
+        
     } else {
-        document.body.insertBefore(newLine(response, "[ERROR]", 0, 1), commandLine);
+        newLine(response, "[ERROR]", 0, "error");
     }
     
 }
 
 async function fetchWeather(CITY_NAME) {
-    const URL = `https://api.weatherapi.com/v1/current.json?key=8a7850118cab40ffa86151016232312&q=${CITY_NAME}&aqi=no`;
+    const URL = `https://api.weatherapi.com/v1/forecast.json?key=8a7850118cab40ffa86151016232312&q=${CITY_NAME}&days=1&aqi=no&alerts=no`;
     const response = await fetch(URL, {mode: "cors"})
                             .then((res) => { 
                                 if (res.ok) return res.json();
